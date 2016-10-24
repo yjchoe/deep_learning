433fa10b6e3179acab71bb0d8c0ec17a2a0e0ab7
yjchoe <yjchoe33@gmail.com>
2016-10-24 04:29:17 -0400

    rbm initial commit

---
+++ hw2/code/NN/nn/algorithm.py
@@ -0,0 +1,22 @@
+"""
+Training algorithms and related classes
+"""
+
+import numpy as np
+
+class LearningRate(object):
+    """Learning rate function class.
+
+    Attributes:
+        const: float
+            Constant term for the learning rate.
+        epoch: int
+            Current epoch number, updated from `NN.train`.
+    """
+    def __init__(self, const, epoch=0):
+        self.lr    = lambda epoch: const / (epoch // 200 + 1.)
+        self.const = const
+        self.epoch = epoch
+
+    def get(self):
+        return self.lr(self.epoch)
